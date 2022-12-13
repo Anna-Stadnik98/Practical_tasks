c0514d3ecead7e619aa5d1828222438a62b33aa6
Anna <st.anuta2013@yandex.ru>
2022-12-13 10:48:11 +0300

    Add Homework4_Z29

---
+++ Homework4_Z29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+// выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] arr = new int[8];
+
+for(int i = 0; i<8; i++)
+{
+    Console.Write("Введите число: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.Write("[");
+System.Console.Write(string.Join(",", arr));
+System.Console.Write("]");
