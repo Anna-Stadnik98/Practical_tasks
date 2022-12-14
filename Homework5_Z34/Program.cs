ccf8c500bce954dde0977e6b2622596581f98619
Anna <st.anuta2013@yandex.ru>
2022-12-14 20:51:59 +0300

    Add Homework5_Z34

---
+++ Homework5_Z34/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 34: Задайте массив заполненный случайными
+положительными трёхзначными числами. Напишите
+программу, которая покажет количество чётных чисел в
+массиве.
+[345, 897, 568, 234] -> 2 */
+
+
+int[] array = new int [4];
+var random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(100, 999);
+}
+
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int count = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if(array[j] % 2 == 0)
+    {
+        count++;
+    }    
+}
+Console.WriteLine($"Количество чётных чисел в массиве:{count}");
