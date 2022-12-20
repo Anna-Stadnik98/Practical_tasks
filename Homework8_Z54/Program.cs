92d6e7b445883f5e78a386ca3286ba40d1572ffb
Anna <st.anuta2013@yandex.ru>
2022-12-20 11:33:59 +0300

    Add Homework8_Z54

---
+++ Homework8_Z54/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+Console.Write("Введите количество строк массива:  ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива:  ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArrayMatrix(rows, columns, 10, 99);
+PrintArray(array);
+Console.WriteLine();
+
+MaxMin(array);
+PrintArray(array);
+
+
+int[,] GetArrayMatrix(int rows, int columns, int minValue = 0, int maxValue = 100)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] MaxMin(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }        
+    }
+    return array;
+} 
