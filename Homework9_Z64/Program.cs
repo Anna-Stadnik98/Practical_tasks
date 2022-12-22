8f97012d472c3eb6edddff72278a109ef393a5c9
Anna <st.anuta2013@yandex.ru>
2022-12-22 14:16:29 +0300

    Add Homework9_Z64

---
+++ Homework9_Z64/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+Console.Write("Введите N:  ");
+int n = int.Parse(Console.ReadLine());
+
+string NumbersRec (int n)
+{
+    if (n > 0) return $"{n}, " + NumbersRec(n-1);
+    else return String.Empty;    
+}
+Console.WriteLine(NumbersRec(n));
