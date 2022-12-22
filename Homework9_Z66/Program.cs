36027d43058e03e4e415b8f2a2749dfc674eaed1
Anna <st.anuta2013@yandex.ru>
2022-12-22 14:16:49 +0300

    Add Homework9_Z66

---
+++ Homework9_Z66/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Write("Введите M:  ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите N:  ");
+int n = int.Parse(Console.ReadLine());
+
+int SumRec (int m, int n)
+{
+    if (m <= n) return n + SumRec(m, n-1);
+    else return 0;
+}
+Console.WriteLine(SumRec(m, n));
